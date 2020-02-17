5138440c7d49260fa2a22280fed0d39d48fa4cdb
pabloturati <pablo.turati@gmail.com>
2020-02-16 21:31:09 -0500

    Implemented private routes for login and backend communication

--- src/atomicDesign/pages/Login/Login.js
+++ src/atomicDesign/pages/Login/Login.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Redirect } from 'react-router-dom'
 import LoginForm from 'atomicDesign/organisms/LoginForm/LoginForm'
 import SignupForm from 'atomicDesign/organisms/SignupForm/SignupForm'
 import ActionButton from 'atomicDesign/atoms/ActionButton/ActionButton'
+import { sessionIsActive } from 'services/sessionStore'
+import { routes } from 'siteData/routes'
 import { useBool } from 'services/hooks'
 import './Login.scss'
 
 const Login = props => {
   const { val: hasCredentials, toggleVal } = useBool(true)
+
+  if (sessionIsActive()) return <Redirect to={routes.profile} />
   return (
     <div className='p__login'>
       {hasCredentials && <LoginForm />}
